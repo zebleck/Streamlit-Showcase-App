a2f0003349ef77115a3384d216cc3da834d71ffe
Fabian Kontor <fabiankontor@aol.com>
2023-09-20 23:27:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Streamlit Showcase App
+
+## Introduction
+
+This repository contains an example Streamlit app that demonstrates some of the capabilities of Streamlit for building data-centric web apps. The app includes features like widgets, caching, data display, and graph plotting.
+
+## Getting Started
+
+### Prerequisites
+
+- Python 3.x
+- Streamlit
+- Pandas
+- Matplotlib
+- NumPy
+
+### Installation
+
+1. Clone this repository:
+   ```bash
+   git clone https://github.com/your_username/Streamlit-Showcase-App.git
+   ```
+2. Navigate to the project repository:
+   ```bash
+   cd Streamlit-Showcase-App
+   ```
+3. Install the required packages:
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+# Running the app
+
+To run the app, execute the following command:
+
+   ```bash
+   streamlit run app.py
+   ```
+
+This will launch the Streamlit app and a new web page should open in your default web browser.
---
+++ app.py
@@ -0,0 +1,47 @@
+# Import necessary libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Function to generate random data (simulate some computational workload)
+@st.cache
+def generate_random_data(n_rows=100):
+    return pd.DataFrame({
+        'A': np.random.rand(n_rows),
+        'B': np.random.randn(n_rows)
+    })
+
+# Main app
+def main():
+    st.title("Streamlit Showcase App")
+
+    st.markdown("### Introduction")
+    st.write("This app showcases some of Streamlit's abilities, including widgets, caching, displaying data, and plotting graphs.")
+
+    st.markdown("### Generate Random Data")
+    n_rows = st.slider("Select the number of rows:", min_value=10, max_value=1000, value=100, step=10)
+    data = generate_random_data(n_rows)
+    
+    st.markdown("### Display Data")
+    if st.checkbox("Show data"):
+        st.dataframe(data)
+
+    st.markdown("### Visualize Data")
+    plot_choice = st.selectbox("Choose a type of plot:", ["Line", "Scatter"])
+    
+    fig, ax = plt.subplots()
+    
+    if plot_choice == "Line":
+        ax.plot(data.index, data['A'], label='Column A')
+        ax.plot(data.index, data['B'], label='Column B')
+    elif plot_choice == "Scatter":
+        ax.scatter(data.index, data['A'], label='Column A')
+        ax.scatter(data.index, data['B'], label='Column B')
+    
+    ax.legend()
+    st.pyplot(fig)
+
+# Run the app
+if __name__ == "__main__":
+    main()
